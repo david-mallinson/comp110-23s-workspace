1f677d0debf78dfb0d49b54bec24202bb4a9a7f1
David Mallinson <davidmal@unc.edu>
2023-03-03 23:12:48 -0500

    ex04

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,42 @@
+def all (list_same: list[int], number: int ) -> bool:
+    elem: int = 0
+    correct: bool = True
+    if len(list_same) == 0:
+        return False
+    while elem < len(list_same) and correct is True:
+        if number == list_same[elem]
+            correct = True
+            elem += 1
+        else:
+            return False
+    return True
+
+
+def is_equal (list_1): list[int], list_2: list[int]) -> bool:
+    elem: int = 0
+    correct: bool = True
+    if len(list_1) != len(list_2)
+        return False
+    while equal is True and (elem < len(list_1) or elem < (list_2)):
+        if list_1[elem] == list_2[elem]
+            correct = True
+            elem += 1
+        else
+            correct = False
+    return correct
+
+
+def max (list_3: list[int]) -> int:
+    if len(list_3) == 0:
+        raise ValueError("max () arg is an empty list")
+    elem: int = 1
+    number: int = list_3[0]
+    while elem < len(list_3)
+        if number > list_3[elem]
+            elem += 1
+        elif number = list_3[elem]
+            number = list_3[elem]
+            n += 1
+        else:
+            n += 1
+    return number
